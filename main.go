f15c6d6a7859c27e96e2b58218f7ab4471712811
carlosmpv <carlosmpvaldes@gmail.com>
2019-09-10 11:14:33 -0300

    adicionando character

--- main.go
+++ main.go
@@ -81,7 +81,7 @@ func main() {
 
 		var goType string
 		switch res.Type {
-		case "char", "bpchar", "varchar", "text", "character varying":
+		case "char", "bpchar", "character", "varchar", "text", "character varying":
 			goType = "*string"
 			break
 		case "smallint":
